7355fa4447738ca4dbe24089a1b8cc5d9adb52ba
aksgh <aksghk4671@gmail.com>
2021-12-15 23:14:34 +0900

    12/15 Profile decorator update

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,13 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from django.urls import reverse
+from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
     model = Profile
     form_class = ProfileCreationForm
@@ -21,6 +25,8 @@ class ProfileCreateView(CreateView):
         return reverse('accountapp:detail', kwargs={'pk': self.object.owner.pk})
 
 
+@method_decorator(login_required, 'get')
+@method_decorator(login_required, 'post')
 class ProfileUpdateView(UpdateView):
     model = Profile
     form_class = ProfileCreationForm
