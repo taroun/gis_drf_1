cc2822c0ff52c5588dab898504027fec53e7529a
aksgh <aksghk4671@gmail.com>
2021-12-17 11:32:56 +0900

    12/17 Article Detail View / Article thumbnail generation update

--- articleapp/urls.py
+++ articleapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from articleapp.views import ArticleCreateView
+from articleapp.views import ArticleCreateView, ArticleDetailView
 
 app_name = 'articleapp'
 
 urlpatterns = [
     path('create/', ArticleCreateView.as_view(), name='create'),
+    path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),
 
 ]
