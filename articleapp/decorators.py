16e9c65294b527c89426592b27972f8fbf8f84e6
aksgh <aksghk4671@gmail.com>
2021-12-17 14:57:15 +0900

    12/17 Article Update / Delete View update

---
+++ articleapp/decorators.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponseForbidden
+
+from articleapp.models import Article
+
+
+def article_ownership_required(func):
+    def decorated(request, *args, **kwargs):
+        target_article = Article.objects.get(pk=kwargs['pk'])
+        if not target_article.writer == request.user:
+            return HttpResponseForbidden()
+        return func(request, *args, **kwargs)
+    return decorated
